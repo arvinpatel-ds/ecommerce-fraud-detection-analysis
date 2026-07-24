faf81215308727534748dcf09a092178831c13d2
Arvin <arvin.patel.ds@gmail.com>
2026-07-25 02:07:18 +0530

    Create business_queries.sql

---
+++ sql/business_queries.sql
@@ -0,0 +1,19 @@
+-- Top 10 product categories by fraud count
+
+-- Fraud rate by payment method
+
+-- Average transaction amount for fraudulent transactions
+
+-- Monthly fraud trend
+
+-- Top customer locations with highest fraud
+
+-- High-risk transaction hours
+
+-- Average account age of fraudulent users
+
+-- Fraud percentage by device
+
+-- Top shipping regions by fraud
+
+-- Rank product categories by fraud amount
